5846c2507a8c6f870216a4d95b468c67b56bf2df
Everone <mainuser@Amrits-MacBook-Pro.local>
2020-12-11 22:20:27 -0800

    Inital Commit

---
+++ Program.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Collections.Generic;
+
+namespace H3VRModInstaller
+{
+    class Program
+    {
+        private static ModList mods = new ModList();
+
+        private static DownloadMods downloader = new DownloadMods();
+        static void Main(string[] args)
+        {
+                if (File.Exists("H3VR.exe"))
+                {
+                    Console.WriteLine("H3VR.exe detected");
+                    Console.WriteLine("Welcome to the H3VR Mod installer!");
+                    Console.WriteLine("Please select the mod you would like to install!");
+                    Console.WriteLine("1: WurstMod");
+                    Console.WriteLine("2: Cursed DLLs");
+                    Console.WriteLine("3: TNHTweaker");
+                    Console.WriteLine("4: All");
+                    Console.Write("Input the mod you would like to install: ");
+                    string input = Console.ReadLine();
+                        if (input == "WurstMod" | input == "Wurst Mod" | input == "wurstmod" | input == "wurst mod" | input == "1")
+                        {
+                            
+                        
+                            Console.WriteLine("WurstMod selected");
+                            if (mods.onlineCheck())
+                            {
+                                downloader.downloadWurstMod(Directory.Exists("BepInEx"));
+                               
+                            }
+                        }
+                        if (input == "CursedDLLs" | input == "Cursed dlls" | input == "curseddlls" | input == "Cursed.dlls" | input == "cursed dll" | input == "curseddll" | input == "2")
+                        {
+                            Console.WriteLine("CursedDLLs selected");
+                            if (mods.onlineCheck())
+                            {
+                                downloader.downloadCursedDlls(Directory.Exists("BepInEx"));
+                            }
+                        }
+                        if (input == "TNHTweaker" | input == "TNH Tweaker" | input == "tnhtweaker" | input == "tnh tweaker" | input == "3")
+                        {
+                            Console.WriteLine("TNHTweaker selected");
+                            if (mods.onlineCheck())
+                            {
+                                downloader.downloadCursedDlls(Directory.Exists("BepInEx"));
+                            }
+                        }
+                        if (input == "All" | input == "all" | input == "4")
+                        {
+                            Console.WriteLine("All selected");
+                            if(mods.onlineCheck())
+                                downloader.downloadAll(Directory.Exists("BepInEx"));
+                        }
+                        else
+                        {
+                            Console.WriteLine("INVALID INPUT");
+                            return;
+                        }
+
+
+                    }
+
+                    
+                    else
+                    {
+                        Console.WriteLine("H3VR.exe not detected!");
+                        return;
+                    }   
+        }
+    }
+
+/*
+    public struct Mods
+    {
+        public string[] Path { get; set; }
+        public string[] Files { get; set; }
+    }
+*/
+    class ModList
+    {
+        public string[] wurstModPaths = {"https://github.com/BepInEx/BepInEx/releases/download/v5.4.4/", "https://github.com/Deli-Counter/Deli/releases/download/v0.2.5/", "https://github.com/Nolenz/WurstMod/releases/download/v2.0.2.0/"};
+        public string[] wurstModFiles = {"BepInEx_x64_5.4.4.0.zip", "Deli-v0.2.5.zip", "WurstMod.deli"};
+        public string[] cursedDllsPaths = {"https://github.com/BepInEx/BepInEx/releases/download/v5.4.4/", "https://github.com/BepInEx/BepInEx.MonoMod.Loader/releases/download/v1.0.0.0/", "https://github.com/drummerdude2003/CursedDlls.BepinEx/releases/download/v1.3/"};
+        public string[] cursedDllsFiles = {"BepInEx_x64_5.4.4.0.zip", "BepInEx.MonoMod.Loader_1.0.0.0.zip", "CursedDlls.BepInEx_v1.3.zip"};
+        public string[] TNHTweakerPaths = {"https://github.com/BepInEx/BepInEx/releases/download/v5.4.4/", "https://github.com/devyndamonster/TakeAndHoldTweaker/releases/download/v1.3.0/"};
+        public string[] TNHTweakerFiles = {"BepInEx_x64_5.4.4.0.zip", "TakeAndHoldTweaker.dll"};
+        public bool onlineCheck()
+        {
+            CheckOnline checkOnline = new CheckOnline();
+
+            if (checkOnline.isOnline("www.github.com"))
+            {
+                Console.WriteLine("GitHub is Online!");
+                return true;
+            }
+
+            else
+            {
+                Console.WriteLine("Github is either down, or no internet is detected!");
+                return false;
+            }
+        }
+    }
+
+    class CheckOnline
+    {
+
+        public bool isOnline(string URL)
+        {
+            Ping ping = new Ping();
+            PingReply reply = ping.Send(URL, 1000);
+
+
+            if(reply.Status == IPStatus.Success)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+
+        }
+
+
+
+    }
+
+    class Downloader
+    {
+        private WebClient downloader = new WebClient();
+
+        public bool downloadMod(string locationOfFile, string fileToDownload)
+        {
+
+            Console.WriteLine("Downloading Mod \"{0}\" from \"{1}{0}\"\n", fileToDownload, locationOfFile);
+            
+
+            downloader.DownloadFile(locationOfFile + fileToDownload, fileToDownload);
+
+            Console.WriteLine("Successfully Downloaded Mod \"{0}\" from \"{1}{0}\"\n", fileToDownload, locationOfFile);
+
+            return true;
+        }
+    }
+
+    class DownloadMods
+    {
+        private Downloader downloader = new Downloader();
+        private ModList mods = new ModList();
+
+
+        public bool downloadWurstMod(bool hasBepInEx)
+        {
+            if(!hasBepInEx)
+                Console.WriteLine("BepInEx not detected! Downloading.");
+                downloader.downloadMod(mods.wurstModPaths[0], mods.wurstModFiles[0]);
+            /*
+            var wurstModDeps = new List<Mods>
+            {
+                new Mods {Path = mods.wurstModPaths, Files = mods.wurstModFiles}
+            };
+            */
+
+            for (int i = 1; i < 3; i++)
+            {
+                downloader.downloadMod(mods.wurstModPaths[i], mods.wurstModFiles[i]);
+            }
+            return true;
+        }
+
+        public bool downloadCursedDlls(bool hasBepInEx)
+        { 
+            if(!hasBepInEx)
+                Console.WriteLine("BepInEx not detected! Downloading.");
+                downloader.downloadMod(mods.wurstModPaths[0], mods.wurstModFiles[0]);
+            for (int i = 1; i < 3; i++)
+            {
+                downloader.downloadMod(mods.cursedDllsPaths[i], mods.cursedDllsFiles[i]);
+            }
+            return true;
+        }
+        public bool downloadTNHTweaker(bool hasBepInEx)
+        { 
+            if(!hasBepInEx)
+                Console.WriteLine("BepInEx not detected! Downloading.");
+                downloader.downloadMod(mods.wurstModPaths[0], mods.wurstModFiles[0]);
+            for (int i = 1; i < 2; i++)
+            {
+                downloader.downloadMod(mods.TNHTweakerPaths[i], mods.TNHTweakerFiles[i]);
+            }
+            return true;
+        }
+        
+        public bool downloadAll(bool hasBepInEx)
+        {
+            if(!hasBepInEx)
+                Console.WriteLine("BepInEx not detected! Downloading.");
+                downloader.downloadMod(mods.wurstModPaths[0], mods.wurstModFiles[0]);
+            for (int i = 1; i < 3; i++)
+            {
+                downloader.downloadMod(mods.wurstModPaths[i], mods.wurstModFiles[i]);
+                downloader.downloadMod(mods.cursedDllsPaths[i], mods.cursedDllsFiles[i]);
+            }
+            for (int i = 1; i < 2; i++)
+            {
+                downloader.downloadMod(mods.TNHTweakerPaths[i], mods.TNHTweakerFiles[i]);
+            }
+            return true;
+        }
+
+
+    }
+}
