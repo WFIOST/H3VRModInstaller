f06cb3f426cefc55cb7843514a98b42fae73d0c2
Frityet <ambhogal01@gmail.com>
2020-12-13 13:50:13 -0800

    Started ModCheck.cs

--- H3VRModInstaller.Backend/Filesys/Common/ModCheck.cs
+++ H3VRModInstaller.Backend/Filesys/Common/ModCheck.cs
@@ -1,31 +1,96 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using H3VRModInstaller.Common;
-using GlobExpressions;
 
 namespace H3VRModInstaller.Filesys.Common
 {
     public class ModCheck
     {
-        private static string[] listOfMods;
-        public static string[] PluginModsToArray()
+        public bool CheckBepInEx()
         {
-            listOfMods = Glob.Files(Directories.pluginsDir, "*.dll").ToArray();
-            
-            Console.WriteLine(listOfMods);
-            
-            
-            return listOfMods;
+            if (Directory.Exists(Directories.bepinexDir))
+            {
+                return true; 
+            }
+            else
+            {
+                return false;
+            }
         }
-
-        public List<string> ModsToList(List<string> listOfMods)
+        public bool CheckSideloader()
         {
-
-
-            return listOfMods;
+            if (File.Exists(Directories.pluginsDir + "H3VR.Sideloader.dll") )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
         
-        
+        public bool CheckDeli()
+        {
+            if (File.Exists(Directories.pluginsDir + "Deli/Deli.Runtime.dll") )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool CheckTNHTweaker()
+        {
+            if (File.Exists(Directories.pluginsDir + "TakeAndHoldTweaker.dll") )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool CheckWurstMod()
+        {
+            if (File.Exists(Directories.modsDirs + "WurstMod.deli") )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool CheckCursedDlls()
+        {
+            if (Directory.Exists(Directories.pluginsDir + "CursedDlls") )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool CheckLSIIC()
+        {
+            if (Directory.Exists(Directories.pluginsDir + "LSIIC") )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public string[] CheckMods(string[] installedMods)
+        {
+
+
+
+            return installedMods;
+        }
     }
 }
