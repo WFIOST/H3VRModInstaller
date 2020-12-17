770b320d4e968a856aa2712afce6f8ad66a5d20f
Frityet <ambhogal01@gmail.com>
2020-12-17 11:43:51 -0800

    Added more features to class Uninstall

--- H3VRModInstaller.Backend/Filesys/Uninstall.cs
+++ H3VRModInstaller.Backend/Filesys/Uninstall.cs
@@ -1,28 +1,78 @@
 using GlobExpressions;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
+using H3VRModInstaller.Filesys.Common;
 
 
 namespace H3VRModInstaller.Filesys
 {
+    public enum Dirs
+    {
+        Mods,
+        Plugins,
+        VirtualObjects,
+        CustomCharacters,
+        Maps
+        
+    }
+    
     public class Uninstall
     {
-        public bool Remove(string modToRemove)
+        string[] dllFilesToDelete = Glob.FilesAndDirectories(@"BepInEx/Plugins/", "**.dll").ToArray();
+        string[] h3ModsToDelete = Glob.FilesAndDirectories(@"Mods/", "**.h3mod").ToArray();
+        string[] hotModsToDelete = Glob.FilesAndDirectories(@"Mods/", "**.hotmod").ToArray();
+        string[] deliModsToDelete = Glob.FilesAndDirectories(@"Mods/", "**.deli").ToArray();
+        public bool Delete(string[] modToRemove, Dirs directory)
         {
-            File.Delete(modToRemove);
+            string dir;
+            switch (directory)
+            {
+                case Dirs.Mods:
+                    dir = "Mods/";
+                    break;
+                case Dirs.Plugins:
+                    dir = "BepInEx/Plugins/";
+                    break;
+                case Dirs.VirtualObjects:
+                    dir = "VirtualObjects/";
+                    break;
+                case Dirs.CustomCharacters:
+                    dir = "TNH_Tweaker/";
+                    break;
+                case Dirs.Maps:
+                    dir = "CustomLevels/";
+                    break;
+
+
+            }
+            File.Delete(modToRemove[0]);
             return true;
         }
 
         public bool DeleteAllMods()
         {
-            string[] dllFilesToDelete = Glob.FilesAndDirectories(@"BepInEx/Plugins/", "**.dll").ToArray();
-            string[] h3ModsToDelete = Glob.FilesAndDirectories(@"Mods/", "**.h3mod").ToArray();
-            string[] hotModsToDelete = Glob.FilesAndDirectories(@"Mods/", "**.hotmod").ToArray();
-            string[] deliModsToDelete = Glob.FilesAndDirectories(@"Mods/", "**.deli").ToArray();
+                
+            DeleteAllDlls();
+            
+            DeleteAllH3Mods();
+            
+            DeleteAllDeliMods();
+            
+            return true;
+        }
+
+        public bool DeleteAllDlls()
+        {
             for (int i = 0; i <= dllFilesToDelete.Length; i++)
             {
                 File.Delete(dllFilesToDelete[i]);
             }
+            return true;
+        }
+
+        public bool DeleteAllH3Mods()
+        {
             for (int i = 0; i <= h3ModsToDelete.Length; i++)
             {
                 File.Delete(h3ModsToDelete[i]);
@@ -31,12 +81,18 @@ namespace H3VRModInstaller.Filesys
             {
                 File.Delete(hotModsToDelete[i]);
             }
+            return true;
+        }
+
+        public bool DeleteAllDeliMods()
+        {
             for (int i = 0; i <= deliModsToDelete.Length; i++)
             {
                 File.Delete(deliModsToDelete[i]);
             }
             return true;
         }
-        
+
+
     }
 }
