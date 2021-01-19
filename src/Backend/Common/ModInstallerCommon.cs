e07194890dd52f91bc882c6ed4d1e0df0f4f87fe
Potatoes <cd64@mail.com>
2021-01-18 20:14:29 -0500

    updated version

--- src/Backend/Common/ModInstallerCommon.cs
+++ src/Backend/Common/ModInstallerCommon.cs
@@ -44,7 +44,7 @@ namespace H3VRModInstaller.Backend.Common
         /// <summary>
         ///     Current version.
         /// </summary>
-        public static Version ModInstallerVersion = new(1, 0, 0);
+        public static Version ModInstallerVersion = new(0, 5, 0);
 
 
         /// <summary>
