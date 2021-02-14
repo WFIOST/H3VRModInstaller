d6aa26ec776b80768c0410f252582d1fe06471a6
Potatoes <cd64@mail.com>
2021-02-13 21:24:31 -0500

    mvp added, utils + tnht updated

--- src/Backend/Filesys/AutoInstaller.cs
+++ src/Backend/Filesys/AutoInstaller.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using H3VRModInstaller.Common;
+using H3VRModInstaller.Filesys;
+using H3VRModInstaller.GUI;
 
-namespace H3VRModInstaller.GUI.Backend.Filesys
+namespace H3VRModInstaller.Filesys
 {
-	class AutoInstaller
+	class AutoInstaller //this is a bit of code i want to write eventually to auto-install mods from a folder. --potatoes
 	{
-
+		string directory = Utilities.GameDirectory + "/ModInstaller Installer";
 	}
 }
